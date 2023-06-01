0655e6fb2b47d6c70f29090aff83b64b26a70d72
Azi <azi@10.10.10.6>
2023-06-01 18:03:07 +0700

    Embeded new navbar

--- src/pages/dashboard/stats/index.js
+++ src/pages/dashboard/stats/index.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import Wrapper from '../../../assets/wrappers/DashboardFormPage'
+import {Button} from'react-bootstrap'
 
 const StatApp = () => {
   return (
-    <div>StatApp</div>
+    <Wrapper>
+        <div className="d-flex justify-content-between align-items-center flex-wrap mb-md-5">
+            <div>
+                <h3 className="card-label fw-bold">Categeories</h3>
+                <span className="form-text text-muted">
+                    Data List Categories
+                </span>
+            </div>
+            <div>
+                <Button className="btn btn-primary font-weight-bold">
+                    Create New
+                </Button>
+            </div>
+        </div>
+    </Wrapper>
   )
 }
 
